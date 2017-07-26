4aedaf1a4603c1cfb9c9444c841651c4d7f66b74
Ashley McKoy <mckoy@mckoy-macbookpro.roam.corp.google.com>
2017-07-26 15:42:19 -0700

    Finished Lab5

--- app/src/main/java/com/diglesia/hw2017mobiledev/lab5/WikipediaArticleListFragment.java
+++ app/src/main/java/com/diglesia/hw2017mobiledev/lab5/WikipediaArticleListFragment.java
@@ -1,6 +1,8 @@
 package com.diglesia.hw2017mobiledev.lab5;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.support.v4.widget.SwipeRefreshLayout;
@@ -10,7 +12,12 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.BaseAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
+import com.android.volley.toolbox.ImageLoader;
+import com.android.volley.toolbox.NetworkImageView;
+
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -18,6 +25,14 @@ public class WikipediaArticleListFragment extends Fragment {
     private ListView mListView;
     private ArticleAdapter mArticleAdapter;
     private SwipeRefreshLayout mSwipeRefreshLayout;
+    private List<Article> mArticles;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setRetainInstance(true);
+    }
+
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
@@ -40,9 +55,22 @@ public class WikipediaArticleListFragment extends Fragment {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
                 Article article = (Article) parent.getAdapter().getItem(position);
-                // Add your click handling code here.
+                Intent i = new Intent(Intent.ACTION_VIEW);
+                i.setData(Uri.parse(article.getURLString()));
+                startActivity(i);
             }
-        });
+                //Article article = (Article) parent.getAdapter().getItem(position);
+                // Add your click handling code here
+
+            });
+
+        if (mArticles != null) {
+            mArticleAdapter.setItems(mArticles);
+        } else {
+            mSwipeRefreshLayout.setRefreshing(true);
+            refreshArticles();
+        }
+
 
         // Load on start. Manually show the spinner.
         mSwipeRefreshLayout.setRefreshing(true);
@@ -55,6 +83,7 @@ public class WikipediaArticleListFragment extends Fragment {
         WikipediaArticleSource.get(getContext()).getArticles(new WikipediaArticleSource.ArticleListener() {
             @Override
             public void onArticleResponse(List<Article> articleList) {
+                mArticles = articleList;
                 // Stop the spinner and update the list view.
                 mSwipeRefreshLayout.setRefreshing(false);
                 mArticleAdapter.setItems(articleList);
@@ -66,6 +95,10 @@ public class WikipediaArticleListFragment extends Fragment {
         private Context mContext;
         private LayoutInflater mInflater;
         private List<Article> mDataSource;
+        private TextView title;
+        private TextView body_text;
+        private NetworkImageView imageView;
+
 
         public ArticleAdapter(Context context) {
             mContext = context;
@@ -99,14 +132,24 @@ public class WikipediaArticleListFragment extends Fragment {
             // Get model item for this row, and generate view for this row.
             final Article article = mDataSource.get(position);
             View rowView = mInflater.inflate(R.layout.list_item_article, parent, false);
+            imageView = (NetworkImageView) rowView.findViewById(R.id.thumbnail);
+            body_text = (TextView) rowView.findViewById(R.id.article_snip);
+            title = (TextView) rowView.findViewById(R.id.article_title);
+            title.setText(article.getTitle());
+            body_text.setText(article.getBodyExtract());
 
             // Add your code here. You'll want to:
             // 1) Get local references to the title TextView and body TextView, and set the
             // article's contents on them.
             // 2) Get a reference to the NetworkImageView, and use the ImageLoader vended by
             // WikipediaArticleSource to set the image.
+            ImageLoader loader = WikipediaArticleSource.get(getContext()).getImageLoader();
+            imageView.setImageUrl(article.getImageURLString(), loader);
+
 
             return rowView;
         }
+
+
     }
 }
